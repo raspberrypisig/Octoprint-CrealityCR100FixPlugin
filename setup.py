85615d2a715814311340ddeb4ed8c5f22021e1d5
Raspberry Pi <raspberrypisig@gmail.com>
2022-04-26 18:38:02 +1000

    changed to black as python formatter

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@
 
 # The plugin's identifier, has to be unique
 from setuptools import setup
+
 plugin_identifier = "crealitycr100fix"
 
 # The plugin's python package, should be "octoprint_<plugin identifier>", has to be unique
@@ -65,8 +66,10 @@ additional_setup_parameters = {}
 try:
     import octoprint_setuptools
 except:
-    print("Could not import OctoPrint's setuptools, are you sure you are running that under "
-          "the same python installation that OctoPrint is installed under?")
+    print(
+        "Could not import OctoPrint's setuptools, are you sure you are running that under "
+        "the same python installation that OctoPrint is installed under?"
+    )
     import sys
 
     sys.exit(-1)
@@ -84,13 +87,12 @@ setup_parameters = octoprint_setuptools.create_plugin_setup_parameters(
     requires=plugin_requires,
     additional_packages=plugin_additional_packages,
     ignored_packages=plugin_ignored_packages,
-    additional_data=plugin_additional_data
+    additional_data=plugin_additional_data,
 )
 
 if len(additional_setup_parameters):
     from octoprint.util import dict_merge
 
-    setup_parameters = dict_merge(
-        setup_parameters, additional_setup_parameters)
+    setup_parameters = dict_merge(setup_parameters, additional_setup_parameters)
 
 setup(**setup_parameters)
